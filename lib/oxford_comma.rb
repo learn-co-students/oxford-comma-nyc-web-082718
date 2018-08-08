a64a364cb7e30b6982f09c02c63ba2990c46eb65
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-08-08 20:03:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length === 1
+    return array[0]
+  end
+  if array.length === 2
+    return array.join(" and ")
+  end
+  if array.length > 2 
+    return "#{array[0, array.length - 1].join(', ')}, and #{array.last}"
+  end
 end
