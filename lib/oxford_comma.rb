fed1f69df1857747fa3280961dea64203651fb34
tomlev816 <thomaslever508@gmail.com>
2018-08-10 11:23:06 -0400

    built oxford comma program

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length > 1
+    array.insert(-2, "and")
+    last = array.pop(2)
+    last = last.join(" ")
+    array.push(last)
+    if array.length > 2
+      return array.join(", ")
+    else
+      return array.join(" ")
+    end
+  end
+  array.join()
 end
