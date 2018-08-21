a860e49a6a7379511079cf66a627c5b1ebf336d2
Olivier Willems <ojwillems@gmail.com>
2018-08-21 23:48:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  if array.size < 2
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    first_array = []
+    second_array = []
+    x = 0
+    while x < array.size
+      if x < array.size - 2
+        first_array.push(array[x])
+        x += 1
+      else
+        second_array.push(array[x])
+        x += 1
+      end
+    end
+    return "#{first_array.join(", ")}, #{second_array.join(", and ")}"
+  end
 end
